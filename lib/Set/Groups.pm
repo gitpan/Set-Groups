880ff5317c2d6c059e902efbacbdf58eefd61856
Jacquelin Charbonnel <jacquelin@cpan.org>
2007-04-20 21:07:59 +0000

    Import of JACQUELIN/Set-Groups-0.4 from CPAN.
    gitpan-cpan-distribution: Set-Groups
    gitpan-cpan-version:      0.4
    gitpan-cpan-path:         JACQUELIN/Set-Groups-0.4.tar.gz
    gitpan-cpan-author:       JACQUELIN
    gitpan-cpan-maturity:     released

--- lib/Set/Groups.pm
+++ lib/Set/Groups.pm
@@ -4,15 +4,15 @@ package Set::Groups ;
 # 
 #           Jacquelin Charbonnel - CNRS/LAREMA
 #  
-#   $Id: Groups.pm 155 2007-04-16 20:49:31Z jaclin $
+#   $Id: Groups.pm 162 2007-04-20 20:56:21Z jaclin $
 #   
 #   ----
 #  
 # 
 # 
 #   ----
-#   $LastChangedDate: 2007-04-16 22:49:31 +0200 (Mon, 16 Apr 2007) $ 
-#   $LastChangedRevision: 155 $
+#   $LastChangedDate: 2007-04-20 22:56:21 +0200 (Fri, 20 Apr 2007) $ 
+#   $LastChangedRevision: 162 $
 #   $LastChangedBy: jaclin $
 #   $URL: https://svn.math.cnrs.fr/jaclin/src/pm/Set-Groups/Groups.pm $
 #  
@@ -28,174 +28,263 @@ use Carp;
 use warnings;
 use strict;
 
-our $VERSION = 0.3 ;
+our $VERSION = 0.4 ;
+my $hfmt = "Set::Groups: HORROR: group '%s' is cyclic, the walk is infinite... Bye"  ;
 
-sub new
+sub new()
 {
-  my ($type,%h) = @_ ;
-  my $this = {
-        "group" => {}
-		} ;
+	my ($type) = @_ ;
+	my $this = {
+		"group" => {}
+		, "debug" => 0
+	} ;
   
-  %h = map { /^-/ ? lc : $_ ;} %h ;
-
-  if (exists $h{"-group"})
-  {
-    $this->{"group"} = $h{"-group"} ;
-	delete $h{"-group"} ;
-  }
-
-  if (exists $h{"-single"})
-  {
-    $this->{"single"} = $h{"-single"} ;
-	delete $h{"-single"} ;
-  }
-
-  $this->{"arg"} = \%h ;
-  
-  bless $this,$type ;
-  return $this ;
+	bless $this,$type ;
+	return $this ;
 }
 
-sub newGroup
+sub setDebug($)
 {
-  my ($this,$group) = @_ ;
-  if (exists $this->{"group"}{$group})
-  {
-    return 0 ;
-  }
-  else
-  {
-    $this->{"group"}{$group} = {} ;
-	return 1 ;
-  }
+	my ($this,$level) = @_ ;
+	$this->{"debug"} = $level ;
 }
 
-sub deleteGroup
+# -----------------
+# Set management
+# -----------------
+
+sub newGroup($)
 {
-  my ($this,$group) = @_ ;
-  if (exists $this->{"group"}{$group})
-  {
-    delete $this->{"group"}{$group} ;
-    return 1 ;
-  }
-  else
-  {
-	return 0 ;
-  }
-}
+	my ($this,$group) = @_ ;
 
-sub getGroups
-{
-  my ($this) = @_ ;
-  return keys %{$this->{"group"}} ;
-}
-
-sub hasGroup
-{
-  my($this,$group) = @_ ;
-  return exists $this->{"group"}{$group} ;
-}
-
-sub addSingleTo
-{
-  my ($this,$single,$group) = @_ ;
-  $this->{"group"}{$group}{"single"}{$single} = 1 ;
-}
-
-sub addGroupTo
-{
-  my ($this,$mgroup,$group) = @_ ;
-
-  $this->{"group"}{$mgroup} = {} unless (exists $this->{"group"}{$mgroup}) ;
-  $this->{"group"}{$group}{"group"}{$mgroup} = 2 ;
-}
-
-sub isOwnSingleOf
-{
-  my ($this,$candidate,$group) = @_ ;
-  
-  return exists $this->{"group"}{$group}{"single"}{$candidate} ;
-}
-
-sub isGroupOf
-{
-  my ($this,$candidate,$group) = @_ ;
-  
-  return exists $this->{"group"}{$group}{"group"}{$candidate} ;
-}
-
-sub _flattenedSinglesOf
-{
-  my ($this,$group) = @_ ;
-  
-  my %flat = () ;
-  %flat = %{$this->{"group"}{$group}{"single"}} if exists $this->{"group"}{$group}{"single"} ;
-  #print "flat:",Dumper(\%flat) ;
-
-  for my $k (keys %{$this->{"group"}{$group}{"group"}})
-  {
-  #print "k:$k\n";
-    my %fs = $this->_flattenedSinglesOf($k) ;
-	#print "fs:",Dumper \%fs ;
-	for my $kk (keys %fs)
+	if (exists $this->{"group"}{$group})
 	{
-  #print "kk:$kk\n" ;
-	  $flat{$kk} = 1 ;
+		return 0 ;
 	}
-  }
-  #print "group:$group ; flat:",Dumper(\%flat) ;
-  return %flat ;
+	else
+	{
+		$this->{"group"}{$group} = {} ;
+		delete $this->{"partition"} if exists $this->{"partition"} ; 
+		return 1 ;
+	}
+}
+
+sub deleteGroup($)
+{
+	my ($this,$group) = @_ ;
+	
+	if (exists $this->{"group"}{$group})
+	{
+		delete $this->{"group"}{$group} ;
+		delete $this->{"partition"} if exists $this->{"partition"} ;
+		return 1 ;
+	}
+	else
+	{
+		return 0 ;
+	}
+}
+
+sub getGroups()
+{
+	my ($this) = @_ ;
+	return keys %{$this->{"group"}} ;
+}
+
+sub getCyclicGroups
+{
+	my($this) = @_ ;
+
+	$this->_walk() unless exists $this->{"partition"} ;
+	return keys(%{$this->{"partition"}{"cyclic"}}) ;
+}
+
+sub getAcyclicGroups
+{
+	my($this) = @_ ;
+
+	$this->_walk() unless exists $this->{"partition"} ;
+	return keys(%{$this->{"partition"}{"acyclic"}}) ;
+}
+
+sub hasGroup($)
+{
+	my($this,$group) = @_ ;
+	return exists $this->{"group"}{$group} ;
+}
+
+# -----------------
+# Group management
+# -----------------
+
+sub addSingleTo($$)
+{
+	my ($this,$single,$group) = @_ ;
+
+	return 0 if exists $this->{"group"}{$group}{"single"}{$single} ;
+	$this->{"group"}{$group}{"single"}{$single} = 1 ;
+	return 1 ;
+}
+
+sub addGroupTo($$)
+{
+	my ($this,$mgroup,$group) = @_ ;
+
+	return 0 if exists $this->{"group"}{$group}{"group"}{$mgroup} ;
+	$this->{"group"}{$mgroup} = {} unless (exists $this->{"group"}{$mgroup}) ;
+	$this->{"group"}{$group}{"group"}{$mgroup} = 2 ;
+	delete $this->{"partition"} if exists $this->{"partition"} ;
+	return 1 ;
+}
+
+sub removeOwnSingleFrom($$)
+{
+	my ($this,$single,$group) = @_ ;
+
+	if ($this->isSingleOf($single,$group))
+	{
+		delete $this->{"group"}{$group}{"single"}{$single} ;
+		return 1 ;
+	}
+	else { return 0 ; }
+}
+
+sub removeGroupFrom($$)
+{
+	my ($this,$sub,$group) = @_ ;
+
+	if ($this->isGroupOf($sub,$group))
+	{
+		delete $this->{"group"}{$group}{"group"}{$sub} ;
+		delete $this->{"partition"} if exists $this->{"partition"} ;
+		return 1 ;
+	}
+	else { return 0 ; }
+}
+
+# This function performs a total walk, if needeed
+# At exit, the partition is always complete
+sub isAcyclic
+{
+	my ($this,$group) = @_ ;
+
+	$this->_walk() unless exists($this->{"partition"}) ;
+	return exists($this->{"partition"}{"acyclic"}{$group}) ;
 }  
 
-sub isSingleOf
+sub isOwnSingleOf($$)
 {
-  my ($this,$candidate,$group) = @_ ;
-  my %fs = $this->_flattenedSinglesOf($group) ;
-  return exists $fs{$candidate} ;
+	my ($this,$candidate,$group) = @_ ;
+	return exists $this->{"group"}{$group}{"single"}{$candidate} ;
+}
+
+sub isGroupOf($$)
+{
+	my ($this,$candidate,$group) = @_ ;
+	return exists $this->{"group"}{$group}{"group"}{$candidate} ;
+}
+
+sub isSingleOf($$)
+{
+	my ($this,$candidate,$group) = @_ ;
+
+	carp sprintf($hfmt,$group) if $this->{"debug"}>0 && !$this->isAcyclic($group) ;
+	my %fs = $this->_flattenedSinglesOf($group) ;
+	return exists $fs{$candidate} ;
 }  
 
-sub getOwnSinglesOf
+sub getOwnSinglesOf($)
 {
-  my ($this,$group) = @_ ;
-  return keys %{$this->{"group"}{$group}{"single"}} ;
+	my ($this,$group) = @_ ;
+	return keys %{$this->{"group"}{$group}{"single"}} ;
 }
 
-sub getGroupsOf
+sub getGroupsOf($)
 {
-  my ($this,$group) = @_ ;
-  return keys %{$this->{"group"}{$group}{"group"}} ;
+	my ($this,$group) = @_ ;
+	return keys %{$this->{"group"}{$group}{"group"}} ;
 }
 
-sub getSinglesOf
+sub getSinglesOf($)
 {
-  my ($this,$group) = @_ ;
-  #print "group:$group\n" ;
-  my %h = $this->_flattenedSinglesOf($group) ;
-  return keys %h ;
+	my ($this,$group) = @_ ;
+
+	carp sprintf($hfmt,$group) if $this->{"debug"}>0 && !$this->isAcyclic($group) ;
+	my %h = $this->_flattenedSinglesOf($group) ;
+	return keys %h ;
 }
 
-sub removeOwnSingleFrom
+# -----------------
+# private methods
+# -----------------
+
+sub _flattenedSinglesOf()
 {
-  my ($this,$single,$group) = @_ ;
-  if ($this->isSingleOf($single,$group))
-  {
-    delete $this->{"group"}{$group}{"single"}{$single} ;
+	my ($this,$group) = @_ ;
+
+	my %flat = () ;
+	%flat = %{$this->{"group"}{$group}{"single"}} 
+	  if exists $this->{"group"}{$group}{"single"} ;
+
+	for my $k (keys %{$this->{"group"}{$group}{"group"}})
+	{
+		my %fs = $this->_flattenedSinglesOf($k) ;
+		for my $kk (keys %fs)
+		{
+			$flat{$kk} = 1 ;
+		}
+	}
+	return %flat ;
+}  
+
+# This function don't perform a total walk
+# At exit, the partition is incomplete
+sub _isAcyclic($$)
+{
+	my ($this,$group,$passed) = @_ ;
+
+	if (exists $passed->{$group})
+	{
+		$this->{"partition"}{"cyclic"}{$group} = 1 ;
+		return 0 ;
+	}
+	my %passed = ( %$passed, $group => 1 ) ;
+
+	for my $k (keys %{$this->{"group"}{$group}{"group"}})
+	{
+		next if exists $this->{"partition"}{"acyclic"}{$k} ;
+		if (exists $this->{"partition"}{"cyclic"}{$k})
+		{
+			$this->{"partition"}{"cyclic"}{$group} = 1 ;
+			return 0 ;
+		}
+		if ($this->_isAcyclic($k,\%passed)==1)
+		{
+			$this->{"partition"}{"acyclic"}{$k} = 1 ;
+		}
+		else
+		{
+			$this->{"partition"}{"cyclic"}{$k} = 1 ;
+			$this->{"partition"}{"cyclic"}{$group} = 1 ;
+			return 0 ;
+		}  
+	}
+	$this->{"partition"}{"acyclic"}{$group} = 1 ;
 	return 1 ;
-  }
-  else { return 0 ; }
-}
+}  
 
-sub removeGroupFrom
+# Perform an inconditionnal walk on the graph
+sub _walk()
 {
-  my ($this,$sub,$group) = @_ ;
-  if ($this->isGroupOf($sub,$group))
-  {
-    delete $this->{"group"}{$group}{"group"}{$sub} ;
-	return 1 ;
-  }
-  else { return 0 ; }
-}
+	my ($this) = @_ ;
+
+	carp "Set::Groups: DEBUG: walking on the graph to find cycles..." if $this->{"debug"}>0 ;
+	delete $this->{"partition"} if exists $this->{"partition"} ;
+	for my $group ($this->getGroups())  
+	{
+		$this->_isAcyclic($group,{}) ;
+	}
+}  
 
 1; 
 
@@ -252,13 +341,19 @@ Create a new group set.
 
 =head1 INSTANCE METHODS
 
+=head3 setDebug
+
+Set a debug level.
+
+  $groups->setDebug(1) ;
+  
 =head2 Set management
 
 =head3 newGroup
 
-Create a new empty group and add it in the set. 
+Create a new empty group and add it into the set. 
 A group is everything which can be a key of a hash.
-NewGroup returns 0 if this group already exists, 1 otherwise.
+Returns 1 on success, 0 otherwise.
   
   $groups->newGroup("a_group") ;
   $groups->newGroup(1) ;
@@ -275,6 +370,18 @@ Return the list of the groups present into the set.
 
   @groups = $groups->getGroups() ; 
 
+=head3 getCyclicGroups
+
+Return the list of the cyclic groups (i.e. self-contained) present into the set.
+
+  @groups = $groups->getGroups() ; 
+
+=head3 getAcyclicGroups
+
+Return the list of the acyclic groups (i.e. not self-contained) present into the set.
+
+  @groups = $groups->getGroups() ; 
+
 =head3 hasGroup
 
 Check if a group is present into the set.
@@ -288,30 +395,36 @@ Check if a group is present into the set.
 Add a single member to a group. 
 A single is everything which can be a key of a hash.
 If the group doesn't exist in the set, it is created.
+Return 1 on success, 0 otherwise.
 
   $groups->addSingleTo("single","a_group") ;
-
-=head3 addSinglesTo
-
-Add a list of single members to a group. If the group doesn't exist in the set, it is created.
-
-  $groups->addSinglesTo(["single1","single2"],"a_group") ;
 
 =head3 addGroupTo
 
 Add a group member to a group. 
 If the embedding group doesn't exist in the set, it is created.
 If the member group doesn't exist in the set, it is created as an empty group.
+Return 1 on success, 0 otherwise.
 
   $groups->addGroupTo("group_member","a_group") ;
   
-=head3 addGroupsTo
+=head3 removeOwnSingleFrom
 
-Add a list of group members to a group. 
-If the embedding group doesn't exist in the set, it is created.
-If the member group doesn't exist in the set, it is created as an empty group.
+Remove an own single from a group. Return 1 on success, 0 otherwise.
 
-  $groups->addGroupsTo(["group_member1","group_member2"],"a_group") ;
+  $groups->removeSingleFrom("single","a_group") ;
+
+=head3 removeGroupFrom
+
+Remove a group member from a group. Return 1 on success, 0 otherwise.
+
+  $groups->removeGroupFrom("a_member_group","a_group") ;
+
+=head3 isAcyclic
+
+Check if a group is acyclic.
+
+  $is_acyclic = $groups->isAcyclic("a_group") ;
   
 =head3 isOwnSingleOf
 
@@ -329,7 +442,9 @@ Check if a group is member of a group.
 
 Check if a single is a (own or not) member of a group.
 
-  $present = $groups->isSingleOf("single","a_group") ;
+  $present = $groups->isSingleOf("single","an_acyclic_group") ;
+
+Warning - Calling this method wich a cyclic group as argument gives a infinite recursion.
 
 =head3 getOwnSinglesOf
 
@@ -345,21 +460,11 @@ Return the list of groups of a group.
 
 =head3 getSinglesOf
 
-Return the list of (own or not) singles of a group.
+Return the list of (own or not) singles of an acyclic group.
 
-  @singles = $groups->getSinglesOf("a_group") ;
+  @singles = $groups->getSinglesOf("an_acyclic_group") ;
 
-=head3 removeOwnSingleFrom
-
-Remove an own single from a group. Return 1 on success, 0 otherwise.
-
-  $groups->removeSingleFrom("single","a_group") ;
-
-=head3 removeGroupFrom
-
-Remove a group member from a group. Return 1 on success, 0 otherwise.
-
-  $groups->removeGroupFrom("a_member_group","a_group") ;
+Warning - Calling this method wich a cyclic group as argument gives a infinite recursion.
 
 =head1 EXAMPLES
 
@@ -395,78 +500,9 @@ where C<@name> means I<group name>, then the following code :
 	    }
 	  }
 	}
+	die "some groups are cyclic" if scalar($groups->getCyclicGroups())>0 ;
 	print join(', ',$groups->getSinglesOf("all")) ;
 
 gives : apache, sophie, jacquelin, lioudmila, mohammed, smmsp, nobody, adm, annette, operator, james, named, adam, halt, root, daemon, piotr
 
 =cut
-=head1 AUTHOR
-
-Jacquelin Charbonnel, C<< <jacquelin.charbonnel at math.cnrs.fr> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to
-C<bug-Set-Groups at rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Set-Groups>.
-I will be notified, and then you'll automatically be notified of progress on
-your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Set-Groups
-
-You can also look for information at:
-
-=over 4
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Set-Groups>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/Set-Groups>
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Set-Groups>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Set-Groups>
-
-=back
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright Jacquelin Charbonnel E<lt> jacquelin.charbonnel at math.cnrs.fr E<gt>
-
-This software is governed by the CeCILL-C license under French law and
-abiding by the rules of distribution of free software.  You can  use, 
-modify and/ or redistribute the software under the terms of the CeCILL-C
-license as circulated by CEA, CNRS and INRIA at the following URL
-"http://www.cecill.info". 
-
-As a counterpart to the access to the source code and  rights to copy,
-modify and redistribute granted by the license, users are provided only
-with a limited warranty  and the software's author,  the holder of the
-economic rights,  and the successive licensors  have only  limited
-liability. 
-
-In this respect, the user's attention is drawn to the risks associated
-with loading,  using,  modifying and/or developing or reproducing the
-software by the user in light of its specific status of free software,
-that may mean  that it is complicated to manipulate,  and  that  also
-therefore means  that it is reserved for developers  and  experienced
-professionals having in-depth computer knowledge. Users are therefore
-encouraged to load and test the software's suitability as regards their
-requirements in conditions enabling the security of their systems and/or 
-data to be ensured and,  more generally, to use and operate it in the 
-same conditions as regards security. 
-
-The fact that you are presently reading this means that you have had
-knowledge of the CeCILL-C license and that you accept its terms.
-
